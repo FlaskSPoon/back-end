6190f31b54abc90641d4e7231184de1613492a61
Seny BALDE <seny.balde1@unchk.edu.sn>
2025-03-27 00:38:05 -1200

    .env

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,17 +3,17 @@ import { join } from 'path';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
  type: 'mysql',
- host: process.env.MYSQLHOST, 
- port: Number(process.env.MYSQLPORT), 
- username: process.env.MYSQLUSER, 
- password: process.env.MYSQLPASSWORD, 
- database: process.env.MYSQLDATABASE,
+//  host: process.env.MYSQLHOST, 
+//  port: Number(process.env.MYSQLPORT), 
+//  username: process.env.MYSQLUSER, 
+//  password: process.env.MYSQLPASSWORD, 
+//  database: process.env.MYSQLDATABASE,
 
-// host: process.env.DB_HOST || 'localhost',
-// port: Number(process.env.DB_PORT) || 3306, 
-// username: process.env.DB_USER || 'root', 
-// password: process.env.DB_PASS || '',
-// database: process.env.DB_NAME || 'flasksPoon',
+host: process.env.DB_HOST || 'localhost',
+port: Number(process.env.DB_PORT) || 3306, 
+username: process.env.DB_USER || 'root', 
+password: process.env.DB_PASS || '',
+database: process.env.DB_NAME || 'flasksPoon',
      synchronize:false,
      entities: [join(__dirname, '**/*.entity{.ts,.js}')],
      extra: {
